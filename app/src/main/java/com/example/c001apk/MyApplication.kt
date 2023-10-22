b93f795100c85450adfec4e936bd40bb42b7587e
bggRGjQaUbCoE <githubacoount56556@proton.me>
2023-10-23 01:22:27 +0800

    support login, loginutil from coolbbsyou

--- app/src/main/java/com/example/c001apk/MyApplication.kt
+++ app/src/main/java/com/example/c001apk/MyApplication.kt
@@ -3,14 +3,23 @@ package com.example.c001apk
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.c001apk.constant.Constants
+import com.example.c001apk.logic.model.CheckResponse
+import com.example.c001apk.logic.model.LoginResponse
 import com.example.c001apk.util.CookieUtil
 import com.example.c001apk.util.PrefManager
 import com.example.c001apk.util.TokenDeviceUtils
 import com.example.c001apk.util.TokenDeviceUtils.Companion.getTokenV2
-import com.google.android.material.color.DynamicColors
+import com.google.gson.Gson
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 import kotlin.concurrent.thread
 
 class MyApplication : Application() {
@@ -20,8 +29,6 @@ class MyApplication : Application() {
 
         context = applicationContext
 
-        genData()
-        requestData()
         AppCompatDelegate.setDefaultNightMode(PrefManager.darkTheme)
         //DynamicColors.applyToActivitiesIfAvailable(this)
     }
@@ -31,30 +38,6 @@ class MyApplication : Application() {
         lateinit var context: Context
     }
 
-    private fun genData() {
-        CookieUtil.deviceCode = TokenDeviceUtils.getLastingDeviceCode(this)
-        CookieUtil.token = CookieUtil.deviceCode.getTokenV2()
-    }
 
-    private fun requestData() {
-        thread {
-            try {
-                val client = OkHttpClient()
-                val request = Request.Builder()
-                    .url("https://api.coolapk.com/v6/account/checkLoginInfo")
-                    .addHeader("X-Requested-With", "XMLHttpRequest")
-                    .addHeader("X-App-Id", "com.coolapk.market")
-                    .build()
-                val response = client.newCall(request).execute()
-                val headers = response.headers
-                val cookies = headers.values("Set-Cookie");
-                val session = cookies[0]
-                val sessionID = session.substring(0, session.indexOf(";"))
-                CookieUtil.SESSID = sessionID
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
 
 }
